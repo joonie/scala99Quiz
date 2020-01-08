0541d7ec820956f12468f3facac49aa82e95c7c4
youngjun.son <>
2020-01-09 01:25:09 +0900

    get last element from List

---
+++ src/main/scala/P01/LastElement.scala
@@ -0,0 +1,19 @@
+package P01
+
+/**
+ * @author 손영준 (youngjun.son@navercorp.com)
+ */
+object LastElement {
+  //제네릭 리스트(T) 타입으로 인자를 받아 제네릭(T) 타입을 리턴한다.
+  def lastRecursive[T](ls: List[T]): T = ls match {
+    case h :: Nil  => h //Nil : null의 친척이며 아무것도 없는 리스트라는 의미
+    case _ :: tail => lastRecursive(tail)
+    case _         => throw new NoSuchElementException
+  }
+
+  def main(args: Array[String]): Unit = {
+    val result = lastRecursive(List(1, 2, 3, 5, 6, 4))
+
+    println("result : "+result)
+  }
+}
